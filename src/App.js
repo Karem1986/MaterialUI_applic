60163d83c56fdf3866a05d6072de03270a06659f
Karem Ortiz <>
2021-04-02 15:12:13 +0200

    tested material ui button-working!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
+import { Button } from '@material-ui/core'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Button  variant='contained'>
+          Learn more
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+  
     </div>
   );
 }
