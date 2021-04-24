09e47feb929f4aac1ecba5cb00b793c143cfb7fa
Karem Ortiz <>
2021-04-24 15:29:21 +0200

    implemented responsive drawer-still testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import InputBase from "@material-ui/core/InputBase";
+import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -27,6 +28,19 @@ import MenuList from "@material-ui/core/MenuList";
 import { Link } from "react-router-dom";
 //Menu
 import Menu from "@material-ui/core/Menu";
+//Drawer on mobile
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -123,6 +137,11 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 15,
     marginRight: "20px",
   },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
 }));
 
 function ElevationScroll(props) {
@@ -142,10 +161,13 @@ const options = [
   "Leidshe Rein: 033- 123-567- 456",
   "Utrecht: 033-456 6789 ",
 ];
-export default function Header() {
-  const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.down("md"))
+export default function Header(props) {
+  const { window } = props;
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
   const classes = useStyles();
+  //Drawer on mobile:
+  const [ mobileOpen, setMobileOpen] = React.useState(false)
   //Dropdown
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
@@ -293,12 +315,43 @@ const tabs = (
             </Menu>
   </React.Fragment>
 )
+//Drawer on mobile handler:
+const handleDrawerToggle = () => {
+  setMobileOpen(!mobileOpen);
+};
+const drawer = (
+  <div>
+    <div className={classes.toolbar} />
+    <Divider />
+    <List>
+      {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        <ListItem button key={text}>
+          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <List>
+      {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        <ListItem button key={text}>
+          <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+  </div>
+);
+
+const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
     <div className={classes.root}>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
           <Toolbar disableGutters>
             <IconButton
+              onClick={handleDrawerToggle}
               edge="start"
               className={classes.menuButton}
               color="inherit"
@@ -306,6 +359,9 @@ const tabs = (
             >
               <MenuIcon />
             </IconButton>
+            <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
             <img
               alt="Janson Makelaars"
               src={logo}
@@ -327,7 +383,40 @@ const tabs = (
             {matches ? null : tabs}
           </Toolbar>
         </AppBar>
-      </ElevationScroll>
+        </ElevationScroll>
+     
+        <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+    </nav>
+     
       <div className={classes.toolbarMargin}></div>
     </div>
   );
